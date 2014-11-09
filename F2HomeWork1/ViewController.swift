f68fb682abc720d70590cba00d00f425ff372a82
sherwoodgithub <stephenemails@gmail.com>
2014-11-09 14:43:13 -0800

    Initial Commit

---
+++ F2HomeWork1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  F2HomeWork1
+//
+//  Created by Stephen on 11/9/14.
+//  Copyright (c) 2014 Sherwood. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
